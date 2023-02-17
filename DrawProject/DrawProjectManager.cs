a7bf79291e8a9d0a548e772bcdc64fd73eb5d7da
user <user@WIN-JV83T09S6R3>
2023-02-17 23:09:28 +0400

    Добавьте файлы проекта.

---
+++ DrawProject/DrawProjectManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Drawing;
+
+namespace DrawProject
+{
+    internal class DrawProjectManager
+    {
+        internal Bitmap ColorFormatting(Bitmap image, int red, int green, int blue)
+        {
+            int r, g, b;
+
+            for (int i = 0; i < image.Width; i++)
+            {
+                for (int j = 0; j < image.Height; j++)
+                {
+                    Color pixelColor = image.GetPixel(i, j);
+
+                    r = pixelColor.R + red;
+                    r = r % 255;
+                    g = pixelColor.G + green;
+                    g = g % 255;
+                    b = pixelColor.B + blue;
+                    b = b % 255;
+
+                    Color newColor = Color.FromArgb(r, g, b);
+
+                    image.SetPixel(i, j, newColor);
+                }
+            }
+
+            return image;
+        }
+
+        internal void SaveFile(Bitmap image)
+        {
+            Console.Write("Enter the file name:");
+            string newImage = Console.ReadLine() + ".jpeg";
+
+            image.Save(newImage);
+
+            Console.WriteLine("Image saved");
+        }
+    }
+}
---
+++ DrawProject/Program.cs
@@ -0,0 +1,53 @@
+﻿using DrawProject;
+using System;
+using System.Drawing;
+
+DrawProjectManager drawProjectManager = new DrawProjectManager();
+
+string command = "";
+
+while (command != "end")
+{
+    Console.Write("Enter location:");
+    string imageLocation = Console.ReadLine();
+
+    Bitmap image = new Bitmap(imageLocation);
+
+    Console.WriteLine($"1.Random Filter\n2.Custom Filter");
+
+start:
+
+    command = Console.ReadLine();
+
+    if (command.Equals("1"))
+    {
+        var random = new Random();
+        int r = random.Next(0, 255);
+        int g = random.Next(0, 255);
+        int b = random.Next(0, 255);
+
+        Bitmap newRandomImage = drawProjectManager.ColorFormatting(image, r, g, b);
+        drawProjectManager.SaveFile(newRandomImage);
+        image.Dispose();
+    }
+
+    else if (command.Equals("2"))
+    {
+        Console.Write("Red:");
+        int red = int.Parse(Console.ReadLine());
+        Console.Write("Green:");
+        int green = int.Parse(Console.ReadLine());
+        Console.Write("Blue:");
+        int blue = int.Parse(Console.ReadLine());
+
+        Bitmap newImage = drawProjectManager.ColorFormatting(image, red, green, blue);
+        drawProjectManager.SaveFile(newImage);
+        image.Dispose();
+    }
+
+    else
+    {
+        Console.Write("Choose the correct command:");
+        goto start;
+    }
+}
